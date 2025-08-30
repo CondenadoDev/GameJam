2577ff6b61e8ccdf8fe88d6c2210721bfcd9e142
CondenadoDev <Camiloosoriodev@gmail.com>
2025-08-30 08:32:11 -0400

    experimental fow system removed

--- Assets/Fow/Scripts/Utils/LayerAttribute.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Fow.Utils
-{
-    /// <summary>
-    /// Attribute to select a single layer.
-    /// </summary>
-    public class LayerPropertyAttribute : PropertyAttribute { }
-}
